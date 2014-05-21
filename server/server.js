6d76eca46e25648cb54a910cae0e0bc035f54beb
Elvira <karashistka@gmail.com>
2014-05-21 13:11:05 +0300

    Add ya

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
 Meteor.startup(function () {
 	Meteor.users.update({"emails.address": "karashistka@yandex.ru"}, {$set: {isAdmin: true}});
+	Meteor.users.update({"emails.address": "yarotska@ya.ru"}, {$set: {isAdmin: true}});
 });
